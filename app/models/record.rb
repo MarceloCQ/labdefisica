d370f5df45c66ba13fe7eb7d6a5c77eb4dc98628
Marcelo <salcedogzz.jm@gmail.com>
2016-10-21 17:44:06 -0500

    Basic model validations

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,24 @@
+# == Schema Information
+#
+# Table name: records
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  group_id   :integer
+#  teacher_id :integer
+#  course     :string
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Record < ActiveRecord::Base
   belongs_to :teacher, inverse_of: :records
   belongs_to :group, inverse_of: :records
   belongs_to :user, inverse_of: :record
   has_many :scores, inverse_of: :record
+
+  validates :teacher, presence: true
+  validates :group, presence: true
+  validates :user, presence: true
+  validates :course, presence: true
 end
