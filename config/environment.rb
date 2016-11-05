4c3b132639cc73a508c1e73f58bc1a99d074506d
Cajet0 <oliver.r.2@hotmail.com>
2016-11-05 00:01:27 -0600

    Confirma emails, idioma en alertas, ya funciona cambiar password, ya funciona enviar email de confirmacion nuevamente. Se agrego rails g devise:views debido a que tomaba como default esas y no podia editarlas en users/etc. Nota:el func de confirma emails esta comentado para evitar enviar en seeds

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,14 @@
+#ActionMailer::Base.delivery_method = :smtp
+#ActionMailer::Base.smtp_settings = {
+#   :tls => true,
+#   :address => "smtp.gmail.com",
+#   :port => 587,
+#   :domain => "gmail.com",
+#   :authentication => :login,
+#   :user_name => "inscripcioneslabfisica@gmail.com",
+#   :password => "CQw-92t-rFz-SkE"
+#}
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,19 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
+  config.action_mailer.delivery_method = :smtp
+
+  #config.action_mailer.smtp_settings = {
+  #  :enable_starttls_auto => true,
+  #  :address => "smtp.gmail.com",
+  #  :port => 587,
+  #  :domain => "gmail.com",
+  #  :authentication => :login,
+  #  :user_name => "inscripcioneslabfisica@gmail.com",
+  #  :password => "CQw-92t-rFz-SkE"
+  #}
 end
